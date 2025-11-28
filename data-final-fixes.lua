436fbff230accd2a660964ae1a2aba3eb7eaefb4
xkyouchoux <xkyouchoux@hotmail.com>
2025-11-28 16:03:58 -0500

    ...

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -158,10 +158,6 @@ data_util.tech_add_effects("asteroid-productivity", {
     {type = "change-recipe-productivity", recipe = "auric-asteroid-crushing", change = 0.1},
 })
 
-data_util.tech_add_effects("transport-belt-capacity-3-Secretas", {
-    {type = "inserter-stack-size-bonus", modifier = 1},
-})
-
 data_util.conditional_modify({
     type = "technology",
     name = "steam-recycler",
